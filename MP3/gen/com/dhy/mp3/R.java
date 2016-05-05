983158d8a1e108b2e71607aefccb5e2f4917b0e5
AndroidOfDHY <lixiaolei@nbdhyu.edu.cn>
2016-05-05 13:38:19 +0800

    the android TextView Test

--- MP3/gen/com/dhy/mp3/R.java
+++ MP3/gen/com/dhy/mp3/R.java
@@ -10,6 +10,10 @@ package com.dhy.mp3;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f040001;
+        public static final int white=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,25 +22,28 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090002;
+        public static final int signer=0x7f090001;
+        public static final int song=0x7f090000;
     }
     public static final class layout {
         public static final int activity_mp3list=0x7f030000;
+        public static final int mp3item=0x7f030001;
     }
     public static final class menu {
-        public static final int mp3_list=0x7f070000;
+        public static final int mp3_list=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
     }
     public static final class style {
         /** 
@@ -60,10 +67,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+        public static final int textview_style=0x7f070002;
     }
 }
--- MP3/src/com/dhy/mp3/Mp3ListActivity.java
+++ MP3/src/com/dhy/mp3/Mp3ListActivity.java
@@ -11,7 +11,7 @@ public class Mp3ListActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mp3list);
+        setContentView(R.layout.mp3item);
     }
 
 
