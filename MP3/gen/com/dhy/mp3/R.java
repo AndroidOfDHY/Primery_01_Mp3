c4223f78cc7a99ef28e89556ece079e403b8458f
AndroidOfDHY <lixiaolei@nbdhyu.edu.cn>
2016-05-05 15:08:16 +0800

    the android Layout nesting

--- MP3/gen/com/dhy/mp3/R.java
+++ MP3/gen/com/dhy/mp3/R.java
@@ -36,15 +36,21 @@ public final class R {
         public static final int stop=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f090008;
-        public static final int itemicon=0x7f090005;
+        public static final int action_settings=0x7f09000e;
+        public static final int itemicon=0x7f09000b;
+        public static final int lasttime=0x7f090006;
         public static final int list=0x7f090000;
-        public static final int next=0x7f090004;
-        public static final int previous=0x7f090001;
-        public static final int signer=0x7f090007;
-        public static final int song=0x7f090006;
-        public static final int srart=0x7f090002;
-        public static final int stop=0x7f090003;
+        public static final int lrc=0x7f090003;
+        public static final int mp3singer=0x7f090002;
+        public static final int mp3title=0x7f090001;
+        public static final int next=0x7f09000a;
+        public static final int playtime=0x7f090004;
+        public static final int previous=0x7f090007;
+        public static final int seekbar=0x7f090005;
+        public static final int signer=0x7f09000d;
+        public static final int song=0x7f09000c;
+        public static final int srart=0x7f090008;
+        public static final int stop=0x7f090009;
     }
     public static final class layout {
         public static final int activity_mp3list=0x7f030000;
