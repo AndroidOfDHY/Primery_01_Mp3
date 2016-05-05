12ccbf4f9321835a299d829b015f7256cf90f5ab
AndroidOfDHY <lixiaolei@nbdhyu.edu.cn>
2016-05-05 14:07:44 +0800

    the android custom view Test

---
+++ MP3/src/com/dhy/utils/LycirView.java
@@ -0,0 +1,21 @@
+package com.dhy.utils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class LycirView extends TextView {
+
+	public LycirView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+	
+	public LycirView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+	
+	public LycirView(Context context) {
+		super(context);
+	}
+
+}
