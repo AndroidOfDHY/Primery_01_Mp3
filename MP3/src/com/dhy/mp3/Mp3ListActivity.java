e356b5ef34970f948ffd07dadb695be3ee83d278
AndroidOfDHY <lixiaolei@nbdhyu.edu.cn>
2016-05-13 11:13:37 +0800

    add custom list adapter framework

--- MP3/src/com/dhy/mp3/Mp3ListActivity.java
+++ MP3/src/com/dhy/mp3/Mp3ListActivity.java
@@ -1,38 +1,48 @@
 package com.dhy.mp3;
 
+import android.R.anim;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 public class Mp3ListActivity extends Activity {
-	private String[] data = { "多情的月光", "假如爱有天意", "年轮" };
-	private ListView listView = null;
+	//private String[] data = { "多情的月光", "假如爱有天意", "年轮" };
+	private ListView listView = null; // 定义全局的ListView
+	// 定义自定义的Adapter
+	public static MyListAdapter listAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.mp3item);
+		setContentView(R.layout.activity_mp3list);
+		init();
+
+	}
+	
+	// 进行初始化
+	private void init() {
+		// 装载ListView组件
+		listView = (ListView)findViewById(android.R.id.list);
+		// 创建Adapter
+		listAdapter = new MyListAdapter();
+		// 建立ListView与Adapter之间的联系
+		listView.setAdapter(listAdapter);
 		
-		//接收数据
-		Intent intent = getIntent();
-		String data = intent.getStringExtra("extra_data");
-		//验证
-		Log.d("TAG", data);
 	}
 
-	// 按back键是启动
-	@Override
-	public void onBackPressed() {
-		Intent intent = getIntent();
-		intent.putExtra("data_return", "Hello, StartActivity");
-		setResult(RESULT_OK,intent);
-		finish(); // 终止Activity的执行
+	public class MyListAdapter extends BaseAdapter{
+		
 	}
+
+
 	
 	
 
--- MP3/src/com/dhy/mp3/StartActivity.java
+++ MP3/src/com/dhy/mp3/StartActivity.java
@@ -5,12 +5,16 @@
  */
 package com.dhy.mp3;
 
+import com.dhy.utils.DataUtils;
+
+import android.R.integer;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
+import android.widget.Toast;
 
 /**
  * 系统启动界面
@@ -29,8 +33,16 @@ public class StartActivity extends Activity {
 
 	private void init() {
 		// 判断是否有SD卡
+		if(!DataUtils.hasSDCard()){
 	       //无：给出提示信息，结束项目；
+			Toast.makeText(context, "手机中没有SD卡！", Toast.LENGTH_LONG).show();
+			finish();
+		}else{
 	       //有：读取XML文件。
+			DataUtils.initData();
+			//验证
+			Log.d("TAG",DataUtils.getAllList().toString());
+		}
 	}
 
 }
