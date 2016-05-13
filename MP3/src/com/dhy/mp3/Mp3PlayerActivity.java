62166f3a7a89382fda03b7e24bc5c4da4bb69ab1
AndroidOfDHY <lixiaolei@nbdhyu.edu.cn>
2016-05-13 16:57:28 +0800

    the inner class test in Mp3PlayerActivity

--- MP3/src/com/dhy/mp3/Mp3PlayerActivity.java
+++ MP3/src/com/dhy/mp3/Mp3PlayerActivity.java
@@ -10,41 +10,60 @@ import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class Mp3PlayerActivity extends Activity{
+public class Mp3PlayerActivity extends Activity {
 
 	private TextView mp3title;
-	private ImageView ivstart,ivprevious,ivnext,ivstop;	
-	private MediaPlayer mp;	
-	private boolean isStop=true,isPause=false;
+	private ImageView ivstart, ivprevious, ivnext, ivstop;
+	private MediaPlayer mp;
+	private boolean isStop = true, isPause = false;
 
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_mp3player);
+		
 		initView();
 	}
-	
+
 	private void initView() {
 		ivstart = (ImageView) findViewById(R.id.start);
 		ivprevious = (ImageView) findViewById(R.id.previous);
 		ivnext = (ImageView) findViewById(R.id.next);
-		ivstop = (ImageView) findViewById(R.id.stop);		
+		ivstop = (ImageView) findViewById(R.id.stop);
 		mp3title = (TextView) findViewById(R.id.mp3title);
-
+		
+		ivstart.setOnClickListener(new startOnClickListener());
+		ivprevious.setOnClickListener(new previousOnClickListener());
+		ivnext.setOnClickListener(new nextOnClickListener());
+		ivstop.setOnClickListener(new stopOnClickListener());
 	}
 
-	public void startmp3(View v){
-		mp3title.setText("开始播放");
+	class startOnClickListener implements OnClickListener {
+		@Override
+		public void onClick(View v) {
+			mp3title.setText("开始播放");
+		}
 	}
-	public void previousmp3(View v){
-		mp3title.setText("上一首歌");
+
+	class previousOnClickListener implements OnClickListener {
+		@Override
+		public void onClick(View v) {
+			mp3title.setText("上一首歌");
+		}
 	}
-	public void stopmp3(View v){
-		mp3title.setText("停止播放");
+
+	class nextOnClickListener implements OnClickListener {
+		@Override
+		public void onClick(View v) {
+			mp3title.setText("下一首歌");
+		}
 	}
-	public void nextmp3(View v){
-		mp3title.setText("下一首歌");
-	}	
+
+	class stopOnClickListener implements OnClickListener {
+		@Override
+		public void onClick(View v) {
+			mp3title.setText("停止播放");
+		}
+	}
 }
